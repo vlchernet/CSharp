f33fae54084384be70256483b892e02bdc825e52
Volodymyr Cherneta <vche@isd.dp.ua>
2018-11-19 16:19:17 +0200

    Add files via upload

---
+++ winapp1/winapp1/MainForm.Designer.cs
@@ -0,0 +1,169 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: vche
+ * Date: 11/9/2018
+ * Time: 12:00 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+ 
+namespace winapp1
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			resources.ApplyResources(this.label1, "label1");
+			this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.label1.Name = "label1";
+			// 
+			// linkLabel1
+			// 
+			resources.ApplyResources(this.linkLabel1, "linkLabel1");
+			this.linkLabel1.Name = "linkLabel1";
+			this.linkLabel1.TabStop = true;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.AllowDrop = true;
+			this.comboBox1.DisplayMember = "1";
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+			resources.GetString("comboBox1.Items"),
+			resources.GetString("comboBox1.Items1"),
+			resources.GetString("comboBox1.Items2"),
+			resources.GetString("comboBox1.Items3"),
+			resources.GetString("comboBox1.Items4"),
+			resources.GetString("comboBox1.Items5"),
+			resources.GetString("comboBox1.Items6")});
+			resources.ApplyResources(this.comboBox1, "comboBox1");
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1SelectedIndexChanged);
+			// 
+			// dateTimePicker1
+			// 
+			resources.ApplyResources(this.dateTimePicker1, "dateTimePicker1");
+			this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Value = new System.DateTime(2018, 11, 15, 0, 0, 0, 0);
+			this.dateTimePicker1.ValueChanged += new System.EventHandler(this.DateTimePicker1ValueChanged);
+			// 
+			// richTextBox1
+			// 
+			resources.ApplyResources(this.richTextBox1, "richTextBox1");
+			this.richTextBox1.Name = "richTextBox1";
+			// 
+			// button1
+			// 
+			resources.ApplyResources(this.button1, "button1");
+			this.button1.Name = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label2
+			// 
+			resources.ApplyResources(this.label2, "label2");
+			this.label2.Name = "label2";
+			// 
+			// label3
+			// 
+			resources.ApplyResources(this.label3, "label3");
+			this.label3.Name = "label3";
+			// 
+			// label4
+			// 
+			resources.ApplyResources(this.label4, "label4");
+			this.label4.Name = "label4";
+			// 
+			// label5
+			// 
+			resources.ApplyResources(this.label5, "label5");
+			this.label5.Name = "label5";
+			// 
+			// label6
+			// 
+			resources.ApplyResources(this.label6, "label6");
+			this.label6.Name = "label6";
+			// 
+			// label7
+			// 
+			resources.ApplyResources(this.label7, "label7");
+			this.label7.Name = "label7";
+			// 
+			// MainForm
+			// 
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.linkLabel1);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.ResumeLayout(false);
+
+		}
+		private System.Windows.Forms.LinkLabel linkLabel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+	}
+}
---
+++ winapp1/winapp1/MainForm.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: vche
+ * Date: 11/9/2018
+ * Time: 12:00 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+
+namespace winapp1
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		const string pattern = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=&&date=&&json";
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void ComboBox1SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string date = dateTimePicker1.Text.Replace(".", "");
+			linkLabel1.Text = pattern;
+			linkLabel1.Text = linkLabel1.Text.Replace("valcode=", "valcode=" + comboBox1.Text);
+			linkLabel1.Text = linkLabel1.Text.Replace("date=", "date=" + date);
+		}
+		void DateTimePicker1ValueChanged(object sender, EventArgs e)
+		{
+			string currency = comboBox1.Text;
+			string date = dateTimePicker1.Text.Replace(".", "");
+			linkLabel1.Text = pattern;
+			linkLabel1.Text = linkLabel1.Text.Replace("valcode=", "valcode=" + currency);
+			linkLabel1.Text = linkLabel1.Text.Replace("date=", "date=" + date);
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			string html = Utility.ReadResponse(linkLabel1.Text);
+			richTextBox1.Text = html;
+			label1.Text = Utility.JsonParse(html);
+			linkLabel1.Text = pattern;
+		}
+	}
+}
